6e0f77cee2ba3521f69587251ccbbe576b2b6e2a
sibzal5 <sibzal5@gmail.com>
2024-01-26 17:19:35 +0300

    commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <div class="container">
+        <div class="header__content">
+            <div class="header__logo">
+                <a href="" class="logo">LibRead</a>
+            </div>
+            <nav class="header__nav">
+                <ul class="header__menu">
+                    <li class="header__item">
+                        <a href="" class="menu-link">Главная</a>
+                    </li>
+                    <li class="header__item">
+                        <a href="" class="menu-link">Каталог</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="header__btns">
+                <a href="" class="btn">Вход</a>
+                <a href="" class="btn">Регистрация</a>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Главная страница')
+
+@section('content')
+    <div class="container">
+        <h1>Всем привет</h1>
+    </div>
+@endsection
